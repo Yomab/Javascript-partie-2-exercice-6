4c3cef1a50cf4450bfa31dfd8e33cadd5dbaec96
Yomab <mabire.yoan@gmail.com>
2019-04-08 14:39:38 +0200

    correction bonus

--- java.js
+++ java.js
@@ -1,4 +1,13 @@
-function passage (element){
-  element.setAttribute('src');
-
+function passage (img){
+  img.src = 'images/' +img.id+'_2.jpg';
 }
+
+function passage2(img){
+img.src = 'images/' +img.id+'.jpg';
+}
+// document.getElementById('image1').addEventListener('mouseover',function(){
+//   passage();
+// });
+// document.getElementById('image1').addEventListener('mouseout' ,function(){
+//
+// });
